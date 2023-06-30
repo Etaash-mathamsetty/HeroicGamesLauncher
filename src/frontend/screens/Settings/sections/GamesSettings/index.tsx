3636243b66ff4c7035a98bdb8cc0f5fe9a47ecb7
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-06-30 10:59:05 -0400

    Update index.tsx

--- src/frontend/screens/Settings/sections/GamesSettings/index.tsx
+++ src/frontend/screens/Settings/sections/GamesSettings/index.tsx
@@ -136,6 +136,8 @@ export default function GamesSettings({ useDetails = true }: Props) {
               <>
                 <EnableFsync />
 
+                <PreferSystemLibs />
+
                 <GameMode />
               </>
             )}
