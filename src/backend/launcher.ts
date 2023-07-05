6279a875781ff42938f4f07b6e101767eb6cd372
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-07-05 08:40:59 -0400

    use heroic process XDG_CONFIG_HOME

--- src/backend/launcher.ts
+++ src/backend/launcher.ts
@@ -325,7 +325,14 @@ function setupWineEnvVars(gameSettings: GameSettings, gameId = '0') {
     ret.DXVK_FRAME_RATE = gameSettings.DXVKFpsCap
   }
   if (gameSettings.showMangohud) {
-    ret.MANGOHUD_CONFIGFILE = join(userHome, '.config/MangoHud/MangoHud.conf')
+    if (!process.env.XDG_CONFIG_HOME) {
+      ret.MANGOHUD_CONFIGFILE = join(userHome, '.config/MangoHud/MangoHud.conf')
+    } else {
+      ret.MANGOHUD_CONFIGFILE = join(
+        process.env.XDG_CONFIG_HOME,
+        'MangoHud/MangoHud.conf'
+      )
+    }
   }
   if (gameSettings.enableEsync && wineVersion.type !== 'proton') {
     ret.WINEESYNC = '1'
